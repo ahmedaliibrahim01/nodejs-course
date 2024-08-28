406e49019ded8b50e95a162917316deb93019124
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-08-28 18:59:58 +0300

    Express setup

---
+++ 2_express_js/express_setup.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use((request, response) => {
+  response.end("Express");
+});
+
+app.listen(3000, () => {
+  console.log("Express wornking");
+});
