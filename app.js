f6daaf29c63f9da1737c34d24e58902265de3710
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-08-28 21:32:24 +0300

    Routing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // express_setup.js
 //require('./2_express_js/express_setup');
 
-
 // middleware.js
-require('./2_express_js/middleware.js');
+//require('./2_express_js/middleware.js');
+
+// routing.js
+require('./2_express_js/routing.js');
