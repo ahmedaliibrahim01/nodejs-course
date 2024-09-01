ad9d02a097fabca024732b56b61e761e8c018375
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-09-01 18:59:56 +0300

    Database connection

---
+++ -14-connection-db/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mysql = require("mysql2");
+const config = require("./config");
+
+const app = express();
+app.set("view engine", "ejs");
+
+const connection = mysql.createConnection(config.db);
+connection.connect((err) => {
+  if (err) {
+    console.log("Connection failed");
+    return;
+  }
+  console.log("Connection successful");
+
+  connection.query("select * from blog", (err, result)=>{
+    console.log(result)
+  });
+});
+
+const path = require("path");
+const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/admin");
+
+app.use("/libs", express.static(path.join(__dirname, "node_modules")));
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(userRoutes);
+
+app.listen(3000, function () {
+  console.log("listening on port 3000");
+});
---
+++ -14-connection-db/config.js
@@ -0,0 +1,10 @@
+const config = {
+  db: {
+    host: "localhost",
+    user: "root",
+    password: "Ahmed12345",
+    database: "blogapp",
+  },
+};
+
+module.exports = config;
