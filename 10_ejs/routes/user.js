14e93e4b12eb4d47c0bb2df2d2d4eb838a6ebbd3
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-09-01 13:19:17 +0300

    Installing ejs

---
+++ 10_ejs/routes/user.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const path = require("path");
+
+router.use("/blogs/:blogid", function(req, res) {
+    res.render("users/blog-details")
+});
+
+router.use("/blogs", function(req, res) {
+    res.render("users/blogs")
+});
+
+router.use("/", function(req, res) {
+    res.render("users/index")
+});
+
+module.exports = router;
