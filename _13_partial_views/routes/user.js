5072f275b12e7112b287e99c612349a99b3ad5fa
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-09-01 16:39:37 +0300

    partial view

--- _13_partial_views/routes/user.js
+++ _13_partial_views/routes/user.js
@@ -2,52 +2,54 @@ const express = require("express");
 const router = express.Router();
 
 const data = {
-  title: "Home",
-  categories: [
-    "Web Geliştirme",
-    "Mobil Uygulamalar",
-    "Veri Analizi",
-    "Programlama",
-  ],
-  blogs: [
-    {
-      blogid: 1,
-      image: "1.jpeg",
-      title: "Komple Uygulamalı Web Geliştirme",
-      description:
-        "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc",
-      homePage: false
-    },
-    {
-      blogid: 2,
-      image: "2.jpeg",
-      title: "Python ile Sıfırdan İleri Seviye Python Programla",
-      description:
-        "Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
-      homePage: true
-    },
-    {
-      blogid: 3,
-      image: "3.jpeg",
-      title:
-        "Python Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+ Sıfırdan İleri Seviye Python Programla",
-      description:
-        "Sıfırdan Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.eri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
-      homePage: true
-    },
-  ],
-};
+    title: "Popüler Kurslar",
+    categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Ofis Uygulamaları"],
+    blogs: [
+        {
+            blogid: 1,
+            baslik: "Komple Uygulamalı Web Geliştirme",
+            aciklama: "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+            resim: "1.jpeg",
+            anasayfa: true,
+            onay: true
+        },
+        {
+            blogid: 2,
+            baslik: "Python ile Sıfırdan İleri Seviye Python Programlama",
+            aciklama: "Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
+            resim: "2.jpeg",
+            anasayfa: true,
+            onay: false
+        },
+        {
+            blogid: 3,
+            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
+            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
+            resim: "3.jpeg",
+            anasayfa: false,
+            onay: true
+        },
+        {
+            blogid: 3,
+            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
+            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
+            resim: "3.jpeg",
+            anasayfa: false,
+            onay: true
+        },
+    ]
+}
 
-router.use("/blogs/:blogid", function (req, res) {
-  res.render("users/blog-details");
+router.use("/blogs/:blogid", function(req, res) {
+    res.render("users/blog-details");
 });
 
-router.use("/blogs", function (req, res) {
-  res.render("users/blogs", data);
+router.use("/blogs", function(req, res) {
+    res.render("users/blogs", data);
 });
 
-router.use("/", function (req, res) {
-  res.render("users/index", data);
+router.use("/", function(req, res) {
+    res.render("users/index", data);
 });
 
 module.exports = router;
