4e483b69cd58ae942b7aac1ddcec882dc507d34b
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-09-02 09:05:16 +0300

    Blog list

--- _14-connection-db/app.js
+++ _14-connection-db/app.js
@@ -25,7 +25,6 @@ const adminRoutes = require("./routes/admin");
 app.use("/libs", express.static(path.join(__dirname, "node_modules")));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
 app.use(userRoutes);
 
 app.listen(3000, function () {
